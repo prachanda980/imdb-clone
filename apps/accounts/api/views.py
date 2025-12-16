4f24d0c530005532687f2048aad25a36358f8463
prachanda <woliprachanda980@gmail.com>
2025-12-16 21:23:33 +0545

    fix: send email after successful login

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from apps.accounts.api.serializers import UserSerializer
+from apps.accounts.tasks import send_welcome_email
 
 from apps.accounts.api.serializers import (
     CustomTokenObtainPairSerializer,
@@ -14,6 +15,36 @@ from apps.accounts.models import CustomUser
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+    def post(self, request, *args, **kwargs):
+        # 1. Standard Login Process
+        response = super().post(request, *args, **kwargs)
+        
+        # 2. Debugging Logic
+        if response.status_code == 200:
+            print("DEBUG: Login successful. Attempting to send email...") # <--- Debug 1
+            
+            user_email = request.data.get('email')
+            print(f"DEBUG: User Email from request: {user_email}") # <--- Debug 2
+            
+            if user_email:
+                from apps.accounts.models import CustomUser
+                try:
+                    user = CustomUser.objects.get(email=user_email)
+                    print(f"DEBUG: User found: {user.name}. Triggering Celery task...") # <--- Debug 3
+                    
+                    # Call the task
+                    send_welcome_email.delay(user.email, user.name)
+                    
+                    print("DEBUG: Task sent to Redis!") # <--- Debug 4
+                    
+                except CustomUser.DoesNotExist:
+                    print("ERROR: User object not found in DB!")
+                except Exception as e:
+                    print(f"ERROR: Celery failed to trigger: {str(e)}")
+            else:
+                print("ERROR: Email field missing in request body!")
+
+        return response
 
 
 # Registration API View
@@ -33,3 +64,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     def get_object(self):
         # Return the logged-in user
         return self.request.user
+
+
+   
