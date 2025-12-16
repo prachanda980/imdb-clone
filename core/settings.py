a904a7e282a581c94ad72baf0a1cd723f20e5e7d
prachanda <woliprachanda980@gmail.com>
2025-12-16 17:07:00 +0545

    add: 'apps' to the system path

--- core/settings.py
+++ core/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Add 'apps' to the system path so Django can find them
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))  
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
