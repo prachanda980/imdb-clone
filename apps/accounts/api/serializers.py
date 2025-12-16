7dc9e68fd66543facf60383ce8972325d2695c23
prachanda <woliprachanda980@gmail.com>
2025-12-16 18:21:19 +0545

    fix : Refactor code

--- apps/accounts/api/serializers.py
+++ apps/accounts/api/serializers.py
@@ -1,20 +1,26 @@
 from rest_framework import serializers
 from apps.accounts.models import CustomUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'name', 'is_active', 'is_staff']
+        fields = ["id", "email", "name", "is_active", "is_staff"]
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     """
     Used for the Registration Endpoint.
     """
+
     password = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True, label="Confirm password")
 
     class Meta:
         model = CustomUser
-        fields = ['email', 'name', 'password','password2']
+        fields = ["email", "name", "password", "password2"]
+
     def validate(self, attrs):
         if attrs.get("password") != attrs.get("password2"):
             raise serializers.ValidationError({"password": "Passwords do not match."})
@@ -23,24 +29,26 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop("password2", None)
         user = CustomUser.objects.create_user(
-            email=validated_data['email'],
-            name=validated_data['name'],
-            password=validated_data['password']
+            email=validated_data["email"],
+            name=validated_data["name"],
+            password=validated_data["password"],
         )
         return user
+
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     """
-    We extend the default JWT serializer to add 'user' details 
+    We extend the default JWT serializer to add 'user' details
     into the response JSON.
     """
+
     def validate(self, attrs):
         # This generates the 'access' and 'refresh' tokens
         data = super().validate(attrs)
-        
+
         # Now we add our custom data
-        data['user'] = {
-            'email': self.user.email,
-            'name': self.user.name,
-            
+        data["user"] = {
+            "email": self.user.email,
+            "name": self.user.name,
         }
         return data
--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -3,14 +3,19 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
-from apps.accounts.api.serializers import CustomTokenObtainPairSerializer, RegisterSerializer
+from apps.accounts.api.serializers import (
+    CustomTokenObtainPairSerializer,
+    RegisterSerializer,
+)
 from apps.accounts.models import CustomUser
+
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
+
 # Registration API View
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = (AllowAny,) 
+    permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
