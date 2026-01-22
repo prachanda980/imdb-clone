fbba39cc2a0355109a7265168740c3647782b47b
prachanda <woliprachanda980@gmail.com>
2026-01-22 17:12:18 +0545

    feat: implement rate limiting and caching for movie api

--- apps/movies/api/views.py
+++ apps/movies/api/views.py
@@ -2,8 +2,11 @@ from rest_framework import viewsets, filters, permissions, status
 from rest_framework.response import Response
 from django.core.cache import cache
 from django.conf import settings
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.exceptions import ValidationError
+from rest_framework.throttling import ScopedRateThrottle
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from apps.movies.models import Movie, Genre, Person, Review, MovieCrew
 from .serializers import (
@@ -19,6 +22,14 @@ class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes = [IsAdminOrReadOnly]
+    
+    @method_decorator(cache_page(60 * 60))  # Cache for 1 hour
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 60))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
 
 class PersonViewSet(viewsets.ModelViewSet):
@@ -27,6 +38,14 @@ class PersonViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     search_fields = ["name"]
     filter_backends = [filters.SearchFilter]
+
+    @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 15))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
 
 class MovieViewSet(viewsets.ModelViewSet):
@@ -99,6 +118,8 @@ class MovieViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = 'review'
 
     def get_queryset(self):
         """
