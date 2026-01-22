3228dc96517ceb5844e9a900714f7f88a2ba9d51
prachanda <woliprachanda980@gmail.com>
2026-01-22 17:12:46 +0545

    feat: add swagger ui and schema endpoints

--- core/urls.py
+++ core/urls.py
@@ -16,14 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.accounts.api.urls')),  
-    path('api/v1/', include('apps.movies.api.urls')),  
-
+    path('api/v1/', include('apps.movies.api.urls')), 
+    
+    # Swagger / OpenAPI
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
